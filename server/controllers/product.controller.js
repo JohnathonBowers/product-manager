5e692d003b08d829f78899bb554210b6d3e7ffe3
Johnathon Bowers <johnathonbowers@gmail.com>
2023-03-10 21:24:05 -0500

    add methods in product.controller.js to create new product and get all products

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,29 @@
+const Product = require("../models.product.model");
+
+// Create new product
+module.exports.createNewProduct = (req, res) => {
+    Product.exists({title: req.body.title})
+        .then(productExists => {
+            if (productExists) {
+                return Promise.reject("Product already exists.");
+            }
+            return Product.create(req.body);
+        })
+        .then(newlyCreatedProduct => {
+            res.json({newlyCreatedProduct})
+        })
+        .catch(err => {
+            res.json({message: "Something went wrong.", error: err})
+        })
+}
+
+// Get all products
+module.exports.findAllProducts = (req, res) => {
+    Product.find()
+        .then(allProducts => {
+            res.json({allProducts})
+        })
+        .catch(err => {
+            res.json({message: "Something went wrong.", error: err})
+        });
+}
