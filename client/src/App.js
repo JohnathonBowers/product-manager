17f89e367013225784413d8129a52ece47b63d1d
Johnathon Bowers <johnathonbowers@gmail.com>
2023-03-18 20:54:59 -0400

    Use Navigate element in App.js to redirect from '/' route to '/products'

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import ProductDetail from './components/ProductDetail';
 import ProductUpdate from './components/ProductUpdate';
 import Main from './views/Main';
@@ -7,6 +7,8 @@ function App() {
   return (
     <div>
       <Routes>
+        {/* I'm using code from https://www.copycat.dev/blog/react-router-redirect/ below for the Route that redirects from the "/" route to the "/products" route */}
+        <Route element={<Navigate to="/products" />} path="/" />
         <Route element={<Main />} path="/products" default />
         <Route element={<ProductDetail />} path="/products/:id" />
         <Route element={<ProductUpdate />} path="/products/edit/:id" />
